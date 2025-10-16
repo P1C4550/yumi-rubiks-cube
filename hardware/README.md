6df0fad838515b9684a8a559203a7c196a9e27fa
Pawel Herok <pawel.herok@gmail.com>
2025-10-16 20:14:55 +0200

    Initial commit; created project - created directory structure and uv projects, they are in their default state

---
+++ hardware/README.md
@@ -0,0 +1,2 @@
+# Hardware
+This *will* contain 3d models, and other hardware things needed for the project
---
+++ software/cube-controller-py/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cube-controller-py!")
+
+
+if __name__ == "__main__":
+    main()
