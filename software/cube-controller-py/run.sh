ffdcdcc43d59295e86affaf8314ac40f66c29e17
PawelNaBCU <pawel.herok+gitbcu@gmail.com>
2025-10-24 16:59:11 +0200

    WIP: rs-lib

---
+++ software/cube-controller-py/run.sh
@@ -0,0 +1,1 @@
+uv run python -c "import solver_core_rs;solver_core_rs.init_table();solver_core_rs.solve_from_faces(\"DRBLUURLDRBLRRBFLFFUBFFDRUDURRBDFBBULDUDLUDLBUFFDBFLRL\",$1,$2,$3)"
--- software/solver-core-rs/src/lib.rs
+++ software/solver-core-rs/src/lib.rs
@@ -1,6 +1,8 @@
 use pyo3::prelude::*;
-use pyo3::exceptions::PyTypeError;
+use pyo3::exceptions::{PyValueError, PyRuntimeError, PyException};
 use once_cell::sync::OnceCell;
+
+pyo3::create_exception!(_core, CubieCreationError, PyException);
 
 // generate the table only once
 static DATA_TABLE: OnceCell<kewb::DataTable> = OnceCell::new();
@@ -12,24 +14,35 @@ fn init_table() {
 
 #[pyfunction]
 #[pyo3(signature = (faces, start_max_moves, stop_max_moves, timeout = None))]
+// max moves is inclusive: [start_max_moves; stop_max_moves]
 fn solve_from_faces(faces: &str, start_max_moves: u8, stop_max_moves: u8, timeout: Option<f32>) -> PyResult<String> {
-	if timeout.is_none() {println!("non :(")}
-
 	let data_table_get = DATA_TABLE.get();
 	let data_table = match data_table_get {
 		Some(v) => v,
-		None => return Err(PyTypeError::new_err("DataTable NOT initialized")),
+		None => return Err(PyRuntimeError::new_err("DataTable not initialized")),
 	};
 	
 	let face_cube_result = kewb::FaceCube::try_from(faces);
 	let face_cube = match face_cube_result {
 		Ok(v) => v,
-		Err(_) => return Err(PyTypeError::new_err("Invalid faces data")),	
+		Err(_) => return Err(PyValueError::new_err("Invalid faces data")),	
 	};
 
-	for max_solver_moves in start_max_moves..stop_max_moves {
-		let solver = kewb::Solver::new(&data_table, max_solver_moves, timeout);
+	let starting_state_result = kewb::CubieCube::try_from(&face_cube);
+	let starting_state = match starting_state_result {
+		Ok(v) => v,
+		Err(_) => return Err(CubieCreationError::new_err("Could not create CubieCube from FaceCube"))
+	};
+	
+	for max_solver_moves in start_max_moves .. stop_max_moves + 1 {
+		let mut solver = kewb::Solver::new(data_table, max_solver_moves, timeout);
 		println!("Made solver with {} moves", max_solver_moves);
+		match solver.solve(starting_state) {
+			Some(solution) => {
+				println!("{}",solution);
+			},
+			None => println!("no solution :(, at least this time")
+		};
 	}
 	
 	Ok("".to_string())
@@ -42,6 +55,8 @@ fn solve_from_faces(faces: &str, start_max_moves: u8, stop_max_moves: u8, timeou
 fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
 	m.add_function(wrap_pyfunction!(init_table, m)?)?;
 	m.add_function(wrap_pyfunction!(solve_from_faces, m)?)?;
+
+	m.add("CubieCreationError", m.py().get_type::<CubieCreationError>())?;
 
     Ok(())
 }
